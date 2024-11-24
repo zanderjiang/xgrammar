c22bd86dbb495ae52556cd99016aaae0a64da009
Yixin Dong <yixind@andrew.cmu.edu>
2024-11-23 20:52:50 -0500

    [Fix] Update config.cmake (#94)
    This PR removes useless config info in config.cmake.

--- cmake/config.cmake
+++ cmake/config.cmake
@@ -1,8 +1,3 @@
 set(CMAKE_BUILD_TYPE RelWithDebInfo)
 set(XGRAMMAR_BUILD_PYTHON_BINDINGS ON)
 set(XGRAMMAR_BUILD_CXX_TESTS OFF)
-# set it to your own architecture
-set(XGRAMMAR_CUDA_ARCHITECTURES
-    native
-    CACHE STRING "CUDA architectures"
-)
