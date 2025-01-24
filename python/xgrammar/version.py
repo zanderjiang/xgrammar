933f6f5e0668980ebad884abea093dffce6a6422
Yixin Dong <yixind@andrew.cmu.edu>
2025-01-23 23:28:54 -0500

    Bump to v0.1.11 (#166)

--- python/xgrammar/version.py
+++ python/xgrammar/version.py
@@ -20,7 +20,7 @@ import subprocess
 
 # ---------------------------------------------------
 
-__version__ = "0.1.10"
+__version__ = "0.1.11"
 PROJ_ROOT = os.path.dirname(os.path.abspath(os.path.expanduser(__file__)))
 
 
